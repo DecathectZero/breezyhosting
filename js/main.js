11b68728f852f9d2e79962df753acf755f416648
Tyler Han <tylerhan97@gmail.com>
2017-05-20 02:15:23 -0400

    added FAQ

--- js/main.js
+++ js/main.js
@@ -19,6 +19,8 @@ $( document ).ready(function() {
         openModal();
     });
 
+    var $submit = $( "#sendcontact" );
+
     $(document)
         .one('focus.autoExpand', 'textarea.autoExpand', function(){
             var savedValue = this.value;
@@ -31,5 +33,71 @@ $( document ).ready(function() {
             this.rows = minRows;
             rows = Math.ceil((this.scrollHeight - this.baseScrollHeight) / 22);
             this.rows = minRows + rows;
+        })
+        .on('click', '#sendcontact', function (e) {
+            e.preventDefault();
+            if($(this).html() == "Submit"){
+                $("#fullform").submit();
+            }else{
+                $('#contactModal').modal('hide');
+            }
+        })
+        .on('submit', '#fullform', function (e) {
+            e.preventDefault();
+            alert("yoloswag");
+            $.ajax({
+                type     : "POST",
+                cache    : false,
+                url      : 'http://breezyhosting.ca/php/contact.php',
+                data     : $('#fullform').serialize(),
+                success  : function(data) {
+                    alert(data);
+                    $("#fullform").slideUp(function(){
+                        $submit.html("Done");
+                        $submit.css('background-color','#0D47A1');
+                        $submit.css('border','none');
+                        $("#success").html("Thank you for your inquiry! We'll get back to you shortly.");
+                    });
+                },
+                error: function(xhr, status, error) {
+                    alert("Oops, something went wrong: "+ error);
+                }
+            });
+        });
+    // Select all links with hashes
+    $('a[href*="#"]')
+    // Remove links that don't actually link to anything
+        .not('[href="#"]')
+        .not('[href="#0"]')
+        .click(function(event) {
+            // On-page links
+            if (
+                location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+                &&
+                location.hostname == this.hostname
+            ) {
+                // Figure out element to scroll to
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                // Does a scroll target exist?
+                if (target.length) {
+                    // Only prevent default if animation is actually gonna happen
+                    event.preventDefault();
+                    $('html, body').animate({
+                        scrollTop: target.offset().top - 50
+                    }, 500, function() {
+                        // Callback after animation
+                        // Must change focus!
+                        var $target = $(target);
+                        $target.focus();
+                        if ($target.is(":focus")) { // Checking if the target was focused
+                            return false;
+                        } else {
+                            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+                            $target.focus(); // Set focus again
+                        };
+                    });
+                }
+            }
         });
 });
